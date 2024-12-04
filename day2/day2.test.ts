f9340a23f4451499700138679d556e0338141bbd
Pearl Latteier <pearlbea@gmail.com>
2024-12-04 10:40:05 -0600

    day 2 solution

---
+++ day2/day2.test.ts
@@ -0,0 +1,60 @@
+import fs from "fs";
+import path from "path";
+
+import {
+  isDecreasing,
+  isIncreasing,
+  isSafeReport,
+  parseFile,
+  numberOfSafeReports,
+  canBeSafe,
+} from "./day2";
+
+const data = fs.readFileSync(path.resolve("day2/test-input.txt"), "utf8");
+
+describe("day2", () => {
+  it("should parse the file", () => {
+    expect(parseFile(data)).toEqual([
+      [7, 6, 4, 2, 1],
+      [1, 2, 7, 8, 9],
+      [9, 7, 6, 2, 1],
+      [1, 3, 2, 4, 5],
+      [8, 6, 4, 4, 1],
+      [1, 3, 6, 7, 9],
+    ]);
+  });
+  it("should check if increasing", () => {
+    expect(isIncreasing([1, 3, 6, 7, 9])).toBeTruthy();
+    expect(isIncreasing([1, 3, 2, 4, 5])).toBeFalsy();
+  });
+  it("should check if decreasing", () => {
+    expect(isDecreasing([7, 6, 4, 2, 1])).toBeTruthy();
+    expect(isDecreasing([1, 3, 6, 7, 9])).toBeFalsy();
+    expect(isDecreasing([1, 3, 2, 4, 5])).toBeFalsy();
+  });
+
+  it("should return false if gap > 3", () => {
+    const report = isSafeReport([1, 2, 7, 8, 9]);
+    expect(report).toBe(false);
+  });
+  it("should return false if no gap", () => {
+    const report = isSafeReport([1, 2, 2, 3, 4]);
+    expect(report).toBe(false);
+  });
+  it("should return false if not increasing or decreasing", () => {
+    const report = isSafeReport([1, 3, 2, 4, 5]);
+    expect(report).toBe(false);
+  });
+
+  it("should return the number of safe reports", () => {
+    const reports = parseFile(data);
+    const safeReports = numberOfSafeReports(reports);
+    expect(safeReports).toBe(2);
+  });
+
+  it("should test saftey with one level removed", () => {
+    expect(canBeSafe([8, 6, 4, 4, 1])).toBe(true);
+    expect(canBeSafe([1, 2, 7, 8, 9])).toBe(false);
+    expect(canBeSafe([9, 7, 6, 2, 1])).toBe(false);
+  });
+});
---
+++ day2/day2.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import path from "path";
+
+const data = fs.readFileSync(path.resolve("day2/input.txt"), "utf8");
+
+export function parseFile(data: string) {
+  const reports = data.split(/\n/).map((report) => {
+    return report.split(" ").map((item) => {
+      return parseInt(item, 10);
+    });
+  });
+  return reports;
+}
+
+export function isIncreasing(array: number[]) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function isDecreasing(array: number[]) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] <= array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function isSafeReport(report: number[]): boolean {
+  for (let i = 0; i < report.length; i++) {
+    const first = report[i];
+    const next = report[i + 1];
+    if (first === next) return false;
+    if (Math.abs(first - next) > 3) return false;
+  }
+
+  if (!isDecreasing(report) && !isIncreasing(report)) {
+    return false;
+  }
+  return true;
+}
+
+export function numberOfSafeReports(reports: number[][]) {
+  let safeReports = 0;
+  for (let i = 0; i < reports.length; i++) {
+    const isSafe = isSafeReport(reports[i]);
+    if (isSafe) {
+      safeReports++;
+    } else if (canBeSafe(reports[i])) {
+      // added for part 2
+      safeReports++;
+    }
+  }
+  return safeReports;
+}
+
+console.log(numberOfSafeReports(parseFile(data))); // 356 part 1, 498 (too big) for part 2
+
+export function canBeSafe(report: number[]) {
+  for (let i = 0; i < report.length; i++) {
+    const copy = report;
+    copy.splice(i, 1);
+    if (isSafeReport(copy)) {
+      return true;
+    }
+  }
+  return false;
+}
